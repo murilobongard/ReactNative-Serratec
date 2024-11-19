4122a5a2aa115a8e78e08001a048d4cae35a0fdd
murilobongard <murilobongard2005@gmail.com>
2024-11-19 17:14:59 -0300

    Melhorando codigo do app

--- TrabalhoFinal-React-Native/App.tsx
+++ TrabalhoFinal-React-Native/App.tsx
@@ -1,25 +1,10 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import LoginScreen from "./src/screens/LoginScreens/LoginScreens";
-import CadastroScreens from "./src/screens/CadastroScreens/CadastroScreens";
-
-const Stack = createNativeStackNavigator();
+import { Rotas } from "./src/Routes/rotas";
 
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen
-          name="Login"
-          component={LoginScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Cadastro"
-          component={CadastroScreens}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
+      <Rotas />
     </NavigationContainer>
   );
 };

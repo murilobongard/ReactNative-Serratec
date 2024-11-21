ca1e4973c1f7b9d15d6d871fb89ef3ec15d6b22e
murilobongard <murilobongard2005@gmail.com>
2024-11-21 17:39:44 -0300

    Novo livroService

--- TrabalhoFinal-React-Native/src/service/api.ts
+++ TrabalhoFinal-React-Native/src/service/api.ts
@@ -1,4 +1,4 @@
 const api = axios.create({
-  baseURL: "https://673f701ba9bc276ec4b891d5.mockapi.io/api/livros",
+  baseURL: "https://673f701ba9bc276ec4b891d5.mockapi.io/api",
 });
 export default api;

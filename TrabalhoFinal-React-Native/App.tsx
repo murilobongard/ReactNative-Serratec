c7bd9ffdbbae6b9c846f5334e2b5f1e96c1a02ae
luffech <lufech@gmail.com>
2024-11-25 04:04:42 -0300

    Monitoramento de Conexão

--- TrabalhoFinal-React-Native/App.tsx
+++ TrabalhoFinal-React-Native/App.tsx
@@ -1,9 +1,11 @@
+import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { Rotas } from "./src/routes/rotas";
-
-const App = () => {
+import MonitoramentoConexao from "./src/service/MonitoramentoDeConexao"; 
+const App: React.FC = () => {
   return (
     <NavigationContainer>
+      <MonitoramentoConexao />
       <Rotas />
     </NavigationContainer>
   );
---
+++ TrabalhoFinal-React-Native/src/service/MonitoramentoDeConexao.tsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from "react";
+import { Alert } from "react-native";
+import NetInfo from "@react-native-community/netinfo";
+
+const MonitoramentoDeConexao: React.FC = () => {
+  const [isConnected, setIsConnected] = useState<boolean>(true);
+
+  useEffect(() => {
+    const unsubscribe = NetInfo.addEventListener(state => {
+      setIsConnected(state.isConnected ?? false);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (!isConnected) {
+      Alert.alert("Sem Conexão", "Você está offline. Verifique sua conexão com a internet.");
+    }
+  }, [isConnected]);
+
+  return (
+    <></>
+  );
+};
+
+export default MonitoramentoDeConexao;
